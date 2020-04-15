489fff09b4f3aadcea13069f79a02dc3947fc64d
LE-S34 <r208790@yahoo.com.tw>
2020-04-15 20:55:53 +0800

    python

---
+++ lesson2/case01.py
@@ -0,0 +1,43 @@
+# -*- coding:UTF-8 -*-
+import keyword
+
+# print("我是中文")
+
+# if a < b :
+#     print("A")
+#     print("B")
+# else:
+#     print("C")
+#     print("D")
+# print("E")
+
+# a=100
+# b=0b101  #二進位
+# c=0o12   #八進位
+# d=0x12   #十六進位
+# print(a,b,c,d)
+
+# 型態
+# a=10
+# b=3.14
+# c=1+2j
+# print(type(a),type(b),c,type(c))
+
+#變數初始值
+# age,name=28,'Edward'
+# print(age,name)
+
+# score=75
+# mark=score>=60
+# print(mark,type(mark))
+#
+# #保留字
+# kw=keyword.kwlist
+# print(kw)
+
+#刪除變數
+# a=10
+# print(a)
+# del a
+# print(a)
+
---
+++ lesson2/case04.py
@@ -0,0 +1,8 @@
+# -*- coding:UTF-8 -*-
+score=75
+print("score =", score)
+print("score", score, sep="=")
+print("score", score, sep=",")
+print("a", end="")
+print("b")
+print("a", "b")
