1db813146e08737ed1af890fc20f1aff41a9ea29
LE-S34 <r208790@yahoo.com.tw>
2020-04-22 21:29:11 +0800

    python

---
+++ lesson3/case11_primr number_2.py
@@ -0,0 +1,9 @@
+def isprime(n):
+    check = True
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            check =False
+            break
+        return check
+for i in range(2, 101):
+    print(i, '質數' if isprime(i) else '')
