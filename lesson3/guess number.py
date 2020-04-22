74b8e8057104e5a27bb26bfda931c2ffc3ab8be5
LE-S34 <r208790@yahoo.com.tw>
2020-04-22 21:08:14 +0800

    python

--- lesson3/guess number.py
+++ lesson3/guess number.py
@@ -2,9 +2,11 @@ import random as r
 ans = r.randint(1, 99)
 min = 0
 max = 100
+count = 7
 
-while True:
-    guess = int(input('請輸入數字 %d ~ %d :' % (min, max)))
+while count > 0:
+    guess = int(input('(%d次)請輸入數字 %d ~ %d :' % (count, min, max)))
+    count -= 1
     if guess <= min or guess >= max:
         print('請重新輸入')
         continue
