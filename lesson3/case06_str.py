b468084f46ad06eb93560198413b2270b722d425
LE-S34 <r208790@yahoo.com.tw>
2020-04-22 19:52:46 +0800

    python

---
+++ lesson3/case06_str.py
@@ -0,0 +1,8 @@
+s = 'she sell sea shell on the sea shore'
+print(s)
+print('有 %s 個 s' % s.count('s'))
+print('有 sea 這幾個字嗎 ? %d' % s.find('sea'))
+
+# 是否都是a-z A-Z的英文字
+# 技巧: 先利用replace去除空白
+print(s.replace(' ', '').isalpha())
